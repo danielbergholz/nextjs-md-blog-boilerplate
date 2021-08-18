0f9bd7730c1c5e2ec2a198fbd55baa0f27a68b50
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-08-18 20:21:06 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { getPosts, IPost } from '../utils/posts'
 
-const Home: NextPage = () => {
+interface IProps {
+  posts: IPost[]
+}
+
+const Home: NextPage<IProps> = ({ posts }) => {
   return (
     <div>
       <Head>
@@ -10,8 +16,29 @@ const Home: NextPage = () => {
       </Head>
 
       <h1>Blog estático com Next.js!</h1>
+
+      {posts.map((post, index) => (
+        <Link key={index} href={`/${post.slug}`}>
+          <a>
+            <h2>{post.title}</h2>
+            <p>{post.date}</p>
+          </a>
+        </Link>
+      ))}
     </div>
   )
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps = () => {
+  // read markdown files
+  const posts = getPosts()
+
+  // return posts
+  return {
+    props: {
+      posts,
+    },
+  }
+}
